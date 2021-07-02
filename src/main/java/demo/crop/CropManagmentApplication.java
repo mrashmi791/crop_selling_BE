fb6c0360024084426474e97ecf6b79fbfd543b67
Amrendra kumar <amrendrainfo791@gmail.com>
2021-07-02 19:54:22 +0530

    Project setup

---
+++ src/main/java/demo/crop/CropManagmentApplication.java
@@ -0,0 +1,13 @@
+package demo.crop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/crop/CropManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.crop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
